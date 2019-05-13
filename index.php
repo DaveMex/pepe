915776c7cdac5a764f01161d976b183725c416b6
David Hernández M <david.hernandez.mendez@gmail.com>
2019-05-13 07:01:27 -0500

    May 13
    Sigue proceso de diseño

--- index.php
+++ index.php
@@ -8,45 +8,24 @@
     <meta name="description" content="Pepe Jiménez, Mexican photographer and photojournalist.">
     <meta name="author" content="David Hernández">
     <title>Pepe Jiménez, fotografía.</title>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:200,400,400i,600,700,900" rel="stylesheet">    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,400,400i,600,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
     <script src="js/bpgdec.js"></script>
     <style>
-@keyframes slidy {
-0% { left: 0%; }
-20% { left: 0%; }
-25% { left: -100%; }
-45% { left: -100%; }
-50% { left: -200%; }
-70% { left: -200%; }
-75% { left: -300%; }
-95% { left: -300%; }
-100% { left: -400%; }
-}
 
-section#slider { overflow: hidden; }
-section#slider figure canvas { width: 20%; float: left; }
-section#slider figure { 
-  position: relative;
-  width: 500%;
-  margin: 0;
-  left: 0;
-  text-align: left;
-  font-size: 0;
-  animation: 30s slidy infinite; 
-}
     </style>
 </head>
 
 <body>
     <div id="container">
         <header>
-            <img src="images/pepe_white.svg" alt="logo Pepe" class="logo">
+            <!-- <img src="images/pepe_white.svg" alt="logo Pepe" class="logo"> -->
             <nav role="navigation">
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="about.html">About</a></li>
                     <li><a href="#galerias">Galleries</a>
-                        <ul class="dropdown">
+                        <!-- <ul class="dropdown">
                             <li><a href='19s.html'>19 de Septiembre</a></li>
                             <li><a href='presidentes.html'>Presidentes</a></li>
                             <li><a href='#'>Cementerios</a></li>
@@ -54,7 +33,7 @@ section#slider figure {
                             <li><a href='#'>Haití (b&w)</a></li>
                             <li><a href='#'>Presente continuo</a></li>
                             <li><a href='#'>Retrato</a></li>
-                         </ul>
+                         </ul> -->
                     </li>
                     <li><a href="#">Prints</a></li>
                     <li><a href="#">News</a></li>
@@ -71,27 +50,11 @@ section#slider figure {
                 <img src="images/13-JMJR7527_01.bpg" alt="Portada 1" class="slide" id="cover">
                 <img src="images/wall_01.bpg" alt="Portada 2" class="slide">
                 <img src="images/hebron_71.bpg" alt="Portada 3" class="slide">
-            
+                <img src="images/JMJR0156_01_med.bpg" alt="Portada 4" class="slide">
+                <img src="images/13-JMJR7527_01.bpg" alt="Portada 1" class="slide">
             </figure>
         </section>
         
-
-        <section id="articulo">
-            <div class="back">
-                <details>
-                    <summary>La memoria y la imagen. <span class="autor">Por: Carlos Beristain.</span></summary>
-                    <div class="texto">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eaque nesciunt iste sit suscipit nihil esse, consectetur iure cumque ut est doloremque expedita soluta sunt mollitia rem impedit assumenda officiis quas nam asperiores, magni vel! Expedita vel, eligendi iure dolorem architecto recusandae, in ratione adipisci repudiandae atque facilis! Soluta, enim?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fugiat eius ipsum ex adipisci esse, blanditiis explicabo debitis quae harum praesentium perspiciatis laudantium asperiores dolor quasi enim pariatur voluptatem. Quod perspiciatis molestias animi qui cum natus nisi, ipsa praesentium voluptates! Quo nam maiores exercitationem excepturi!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aspernatur quae earum deserunt veniam, commodi facere ipsum at ducimus sunt doloremque, provident voluptas? Quam similique, soluta recusandae labore mollitia quibusdam hic modi fuga unde perspiciatis tempore impedit doloremque expedita reprehenderit, amet at assumenda aut ab. In, expedita. Reprehenderit deserunt molestiae cupiditate repellendus quo deleniti suscipit nihil velit eaque mollitia quae corporis expedita, veritatis iste possimus assumenda officiis impedit laboriosam vitae voluptatibus sunt ad sit porro.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id quidem quae rem eius assumenda quaerat pariatur veritatis dolorem nobis quia velit corporis libero mollitia, hic amet nostrum officiis harum modi?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio recusandae odit laborum assumenda, modi dolores odio veritatis animi, quam ullam ad commodi voluptas repellendus consequatur nihil? Unde quas ex praesentium odit veritatis temporibus earum nobis.</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error hic, veniam, vel soluta nemo repellat iusto corrupti, quam eveniet numquam assumenda nobis mollitia rerum distinctio deserunt id voluptatibus. Voluptatum dicta corrupti excepturi et debitis voluptas, nihil quia tempora quisquam temporibus, quis expedita facere neque dolorem eos eius. Quaerat, corporis at.</p>
-                    <p>This is where you put the details that are shown once expanded</p>
-                </div>
-                </details>
-            </div>
-        </section>
         <section id="galerias" class="indice-portafolio">
             <h3>Galerías.</h3>
             <ul class="indice-galeria">
